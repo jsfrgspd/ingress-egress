6ec5a08c0f8d41067aabec03cf07201da9b3ef64
jsfrgspd <ajt.gaspado@gmail.com>
2024-08-19 16:39:57 +0800

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+    theme: {
+      extend: {
+        borderRadius: {
+          'custom-15': '15px',
+          'custom-8': '8px',
+        },
+        fontFamily: {
+          mont: ['Mont', 'sans-serif'],
+        },
+      }
+    },
+  variants: {},
+  plugins: [],
+}
